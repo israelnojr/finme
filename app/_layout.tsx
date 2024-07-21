a6b6988c5997f3c61759630b7737468326dfee6a
israelnojr <israeliloba@gmail.com>
2024-07-20 18:40:29 -0700

    First Commit, welcome and register screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,13 @@
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Link, router, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -46,14 +47,43 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name='index' options={{headerShown: false}} />
+      <Stack.Screen name='register' options={{
+          title: '',
+          headerBackTitle: '',
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: Colors.background },
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+        }} 
+      />
+      <Stack.Screen
+        name="login"
+        options={{
+          title: '',
+          headerBackTitle: '',
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: Colors.background },
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <Link href={'/help'} asChild>
+              <TouchableOpacity>
+                <Ionicons name="help-circle-outline" size={34} color={Colors.dark} />
+              </TouchableOpacity>
+            </Link>
+          ),
+        }}
+      />
+    </Stack>
   );
 }
---
+++ app/register.tsx
@@ -0,0 +1,88 @@
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { Link, useRouter } from 'expo-router';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+
+const register = () => {
+  const [countryCode, setCountryCode] = useState('+234');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const keyboardVerticalOffset = Platform.OS === 'ios' ? 80 : 0;
+  const router = useRouter();
+
+  const onSignup = async () => {}
+  return (
+    <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior="padding"
+        keyboardVerticalOffset={keyboardVerticalOffset}>
+      <View style={defaultStyles.container}>
+          <Text style={defaultStyles.header}>Let's get started!</Text>
+          <Text style={defaultStyles.descriptionText}>
+            Enter your phone number. We will send you a confirmation code there
+          </Text>
+          <View style={st.inputContainer}>
+            <TextInput
+              style={st.input}
+              placeholder="Country code"
+              placeholderTextColor={Colors.gray}
+              value={countryCode}
+              onChangeText={setCountryCode}
+            />
+            <TextInput
+              style={[st.input, { flex: 1 }]}
+              placeholder="Mobile number"
+              placeholderTextColor={Colors.gray}
+              keyboardType="numeric"
+              value={phoneNumber}
+              onChangeText={setPhoneNumber}
+            />
+          </View>
+          <Link href={'/login'} replace asChild>
+            <TouchableOpacity>
+              <Text style={defaultStyles.textLink}>Already have an account? Log in</Text>
+            </TouchableOpacity>
+          </Link>
+          <View style={{ flex: 1 }} />
+          <TouchableOpacity
+            style={[
+              defaultStyles.pillButton,
+              phoneNumber !== '' ? st.enabled : st.disabled,
+              { marginBottom: 20 },
+            ]}
+            onPress={onSignup}>
+          <Text style={defaultStyles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
+
+const st = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: 'row',
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
+export default register
